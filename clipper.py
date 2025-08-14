4b30a923161072104bfe63cfb6d77bcfaee361b6
Misaka610 <74341@misaka-mikoto.com>
2025-08-14 16:48:53 +0800

    图片裁剪+旋转

---
+++ clipper.py
@@ -0,0 +1,109 @@
+import cv2
+import numpy as np
+
+
+# 固定尺寸
+def resizeImg(image, height=1200):
+    h, w = image.shape[:2]
+    pro = height / h
+    size = (int(w * pro), int(height))
+    img = cv2.resize(image, size)
+    return img
+
+
+# 边缘检测
+def getCanny(image):
+    # 高斯模糊
+    binary = cv2.GaussianBlur(image, (3, 3), 2, 2)
+    # 边缘检测
+    binary = cv2.Canny(binary, 60, 240, apertureSize=3)
+    # 膨胀操作，尽量使边缘闭合
+    kernel = np.ones((3, 3), np.uint8)
+    binary = cv2.dilate(binary, kernel, iterations=1)
+    return binary
+
+
+# 求出面积最大的轮廓
+def findMaxContour(image):
+    # 寻找边缘
+    contours = cv2.findContours(image, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)[0]
+    # 计算面积
+    max_area = 0.0
+    max_contour = []
+    for contour in contours:
+        currentArea = cv2.contourArea(contour)
+        if currentArea > max_area:
+            max_area = currentArea
+            max_contour = contour
+    return max_contour, max_area
+
+
+# 多边形拟合凸包的四个顶点
+def getBoxPoint(contour):
+    # 多边形拟合凸包
+    hull = cv2.convexHull(contour)
+    epsilon = 0.02 * cv2.arcLength(contour, True)
+    approx = cv2.approxPolyDP(hull, epsilon, True)
+    approx = approx.reshape((len(approx), 2))
+    return approx
+
+
+# 适配原四边形点集
+def adaPoint(box, pro):
+    box_pro = box
+    if pro != 1.0:
+        box_pro = box/pro
+    box_pro = np.trunc(box_pro)
+    return box_pro
+
+
+# 四边形顶点排序，[top-left, top-right, bottom-right, bottom-left]
+def orderPoints(pts):
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
+    rect[0] = pts[np.argmin(s)]
+    rect[2] = pts[np.argmax(s)]
+    diff = np.diff(pts, axis=1)
+    rect[1] = pts[np.argmin(diff)]
+    rect[3] = pts[np.argmax(diff)]
+    return rect
+
+
+# 计算长宽
+def pointDistance(a, b):
+    return int(np.sqrt(np.sum(np.square(a - b))))
+
+
+# 透视变换
+def warpImage(image, box):
+    w, h = pointDistance(box[0], box[1]), \
+           pointDistance(box[1], box[2])
+    dst_rect = np.array([[0, 0],
+                         [w - 1, 0],
+                         [w - 1, h - 1],
+                         [0, h - 1]], dtype='float32')
+    M = cv2.getPerspectiveTransform(box, dst_rect)
+    warped = cv2.warpPerspective(image, M, (w, h))
+    return warped
+
+def clip(filepath, outpath):
+    image = cv2.imread(filepath)
+    ratio = 1200 / image.shape[0]
+    # 尺寸变化
+    img = resizeImg(image)
+    # 边缘检测
+    binary_img = getCanny(img)
+    # 最大轮廓检测
+    max_contour, max_area = findMaxContour(binary_img)
+    # 轮廓四顶点拟合
+    boxes = getBoxPoint(max_contour)
+    # 还原原顶点
+    boxes = adaPoint(boxes, ratio)
+    # 顶点排序
+    boxes = orderPoints(boxes)
+    # 透视变化
+    warped = warpImage(image, boxes)
+    cv2.imwrite(outpath, warped)
+
+if __name__ == '__main__':
+    clip('20250813110133_6.jpg','20250814.jpg')
